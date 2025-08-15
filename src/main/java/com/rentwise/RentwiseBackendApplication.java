139d8b8289d91093e42bd46294ed229275ffad0a
amaanlari <amaanlari@outlook.comm>
2025-08-15 15:30:45 +0530

    Initial commit

---
+++ src/main/java/com/rentwise/RentwiseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rentwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentwiseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentwiseBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rentwise/RentwiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentwiseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
